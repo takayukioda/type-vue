93e50ef6f4bd6f80e7bfe26388f3733a70b34f4b
takayukioda <takayuki@odashi.me>
2018-02-10 12:36:38 +0900

    feat: introduce single file component

---
+++ src/components/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div class="greeting">Hello {{ name }}{{ exclamationMarks }}</div>
+		<button @click="decrement">-</button>
+		<button @click="increment">+</button>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+	props: ['name', 'initialEnthusiasm'],
+	data() {
+		return {
+			enthsiasm: this.initialEnthusiasm,
+		}
+	},
+	methods: {
+		increment() {
+			this.enthsiasm++;
+		},
+		decrement() {
+			if (this.enthsiasm > 1) {
+				this.enthsiasm--;
+			}
+		}
+	},
+	computed: {
+		exclamationMarks(): string {
+			return Array(this.enthsiasm + 1).join("!");
+		}
+	}
+});
+</script>
+
+<style>
+.greeting {
+	font-size: 20px;
+}
+</style>
