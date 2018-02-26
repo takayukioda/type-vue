c30e68e08f02372848115cb519c12973e096de94
takayukioda <takayuki@odashi.me>
2018-02-27 02:09:56 +0900

    feat: add configuration for tslint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HellowWorld from '@/components/Hello';
+import HelloWorld from '@/components/Hello.vue';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
 	routes: [
 		{
 			path: '/',
-			name: 'HellowWorld',
-			component: HellowWorld
+			name: 'HelloWorld',
+			component: HelloWorld
 		}
 	]
 });
